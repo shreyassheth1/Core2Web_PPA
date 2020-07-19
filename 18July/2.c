d521c66b0988650a102dfc909272f3909146977e
shreyassheth1 <shreyas.sheth1@gmail.com>
2020-07-20 00:36:26 +0530

    nested for

---
+++ 18July/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n =0;
+	int m=0;
+	scanf("%d %d",&n,&m);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			printf("A ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
